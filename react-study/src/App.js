365fef0ae9ad8b52940f520b9857ab72e5069aec
gorilla-Kim <00ghks22@naver.com>
2020-01-04 19:21:18 +0900

    react Es6문법 JSX 에 대하여 알아보기 fragment : <></> map 문법

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+const exampleData = [
+  {
+    title: "React",
+    value: 0
+  },
+  {
+    title: "Anguler",
+    value: 0
+  },
+  {
+    title: "Express",
+    value: 0
+  }
+]
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          exampleData.map(key => (
+            <>
+            <p key={key.value}>title: {key.title} | value: {key.value}</p>
+            </>
+          ))
+        }
       </header>
     </div>
   );
